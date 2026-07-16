6acfa32779545232646f8678da70ae676ba3a2f6
vishrutcogwave <vih5gamer@gmail.com>
2026-07-16 18:37:36 +0530

    new updated

--- vite-project/src/dummyPackages.ts
+++ vite-project/src/dummyPackages.ts
@@ -1,7 +1,7 @@
 import type { PackageData } from "./types";
 import img1 from "./assets/img1.png";
 import img2 from "./assets/img2.png";
-import img3 from "./assets/img3.png";
+import img3 from "./assets/img6.png";
 import img4 from "./assets/img4.png";
 import img5 from "./assets/img5.png";
 
